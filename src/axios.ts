153e97f95f8252d47f221a34d41c8ccdc4318a5f
Yeldar <57894795+issa012@users.noreply.github.com>
2023-08-23 18:12:32 +0600

    url

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const URL = import.meta.env.VITE_URL;
+export const URL = 'https://rsp-aj41.onrender.com';
 
 export const axiosInstance = axios.create({
   withCredentials: true,
